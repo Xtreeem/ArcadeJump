7fc320a95db67509e0eaef006dc49e4c205f46a6
WiJeBe <wiberg_1@hotmail.com>
2014-05-12 13:11:48 +0200

    Hej

--- ArcadeJump/ArcadeJump/Managers/LevelManager.cs
+++ ArcadeJump/ArcadeJump/Managers/LevelManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +9,15 @@ namespace ArcadeJump
 {
     class LevelManager
     {
+        List<Platform> platformList;
+        public LevelManager(List<Platform> list)
+        {
+            this.platformList = list;
+        }
+
+        public void Update(GameTime gameTime, double gameTimer)
+        { }
+
+       
     }
 }
