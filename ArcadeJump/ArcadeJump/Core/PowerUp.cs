8c00e7f7c96409cc837e9e337bfb3b991d05d630
Xtreeem <hugo.tinglof@gmail.com>
2014-05-12 11:02:20 +0100

    WOrk on the manager

--- ArcadeJump/ArcadeJump/Core/PowerUp.cs
+++ ArcadeJump/ArcadeJump/Core/PowerUp.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace ArcadeJump
 {
-    class PowerUp
+    class PowerUp : AnimatedGameObject
     {
     }
 }
--- ArcadeJump/ArcadeJump/Managers/Manager.cs
+++ ArcadeJump/ArcadeJump/Managers/Manager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +10,26 @@ namespace ArcadeJump
 
     //To.do 
     // Wait for platforms and player to try to manage
+    // -Remember to swap surfaceRectangle to fullsized rectangle in the platform/platform cocllision.
     class Manager
     {
         #region Variables
+        Random Rand;
+        List<Platform> Platforms;
+        List<Player> Players;
+        List<PowerUp> PowerUps;
         #endregion
 
         #region Public Methods
+        public Manager()
+        {
+            Rand = new Random();
+        }
+
+        public void Update(GameTime GameTime)
+        {
+        }
+
         #endregion
 
         #region Private Methods
@@ -37,13 +53,57 @@ namespace ArcadeJump
                 }
                 else if (ObjectB is Player)
                 {
-
+                    //Collision with player and player
                 }
 
                 else if (ObjectB is PowerUp)
                 {
-
+                    //Collision with player and powerup
                 }
+            }
+            else if (ObjectA is Platform)
+                if (ObjectB is PowerUp && ObjectB.velocity.Y > 0)
+                {
+                    //Collision with platform and powerup
+                }
+                else if (ObjectB is Platform)
+                {
+                    if ((ObjectA as Platform).SurfaceRectangle.Contains((ObjectB as Platform).SurfaceRectangle))
+                    {
+                        if (Rand.Next(0, 2) > 0)
+                        {
+                            ObjectA.isDead = true;
+                        }
+                        else
+                            ObjectB.isDead = true;
+                    }
+                }
+        }
+
+        private void RemoveDeadStuff()
+        {
+            for (int i = 0; i < Platforms.Count; i++)
+            {
+                if (Platforms[i].isDead)
+                    Platforms.RemoveAt(i);
+            }
+            for (int i = 0; i < PowerUps.Count; i++)
+            {
+                if (PowerUps[i].isDead)
+                    PowerUps.RemoveAt(i);
+            }
+            for (int i = 0; i < Players.Count; i++)
+            {
+                if (Players[i].isDead)
+                    Players.RemoveAt(i);
+            }
+        }
+
+        private void DrawStuff(SpriteBatch SpriteBatch)
+        {
+            foreach (Platform p in Platforms)
+            {
+                p.Draw(SpriteBatch);
             }
         }
     }
