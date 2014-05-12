35b40f01b10f2b22669d25d67856b14f2a3e96eb
WiJeBe <wiberg_1@hotmail.com>
2014-05-12 14:21:38 +0200

    rectangle

--- ArcadeJump/ArcadeJump/Core/Platform.cs
+++ ArcadeJump/ArcadeJump/Core/Platform.cs
@@ -20,13 +20,14 @@ namespace ArcadeJump
             surfaceHeight = tex.Height * 0.2f;
             source = new Rectangle(0, 0, tex.Width * (int)changeWidth, tex.Height);
             Hitbox = source;
-            SurfaceRectangle = new Rectangle(0, 0, (int)changeWidth *tex.Width, (int)surfaceHeight);
+            SurfaceRectangle = new Rectangle((int)pos.X, (int)pos.Y, (int)changeWidth *tex.Width, (int)surfaceHeight);
             velocity = new Vector2(0, 4);
         }
 
         public override void Update(GameTime gameTime)
         {
             position += velocity;
+            SurfaceRectangle = new Rectangle((int)position.X, (int)position.Y, (int)changeWidth *texture.Width, (int)surfaceHeight);
         }
 
         public override void Draw(SpriteBatch sB)
